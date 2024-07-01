7f654e71fe8551256182220308600663cea2511c
HP <lavina28daryanani@gmail.com>
2024-07-01 23:38:30 +0530

    save changes

--- lib/artist.dart
+++ lib/artist.dart
@@ -1,3 +1,4 @@
+import 'package:spotify/UiHelper.dart';
 import 'package:flutter/material.dart';
 
 class Artist extends StatefulWidget {
@@ -8,45 +9,192 @@ class Artist extends StatefulWidget {
 }
 
 class _ArtistState extends State<Artist> {
+  var arrContent = [
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+    {
+      "image":
+          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+      "songname": "Lorem Ipsum1",
+      "threedot": Icons.more_vert,
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Container(
-          //   foregroundDecoration: BoxDecoration(
-          //     image: DecorationImage(
-          //         image: NetworkImage(
-          //             'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAw1_00y4YhsVN6tmQly3a-tO1igM8i1-K8Q&s'),
-          //         fit: BoxFit.fill),
-          //   ),
-          // decoration: const BoxDecoration(
-          //   image: DecorationImage(
-          //       alignment: Alignment(-.2, 0),
-          //       image: NetworkImage(
-          //           'http://www.naturerights.com/blog/wp-content/uploads/2017/12/Taranaki-NR-post-1170x550.png'),
-          //       fit: BoxFit.cover),
-          // ),
-          // )
-          Stack(children: [
-            Container(
-              child: Image.network(
-                "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
-                fit: BoxFit.fill,
+      // appBar: AppBar(
+      //
+      // ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Stack(alignment: Alignment.bottomLeft, children: [
+              Container(
+                child: Image.network(
+                  "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+                  fit: BoxFit.fill,
+                ),
+                width: double.infinity,
+                height: 200,
               ),
-              width: double.infinity,
-              height: 200,
-            ),
-            Align(
-                    alignment: FractionalOffset.bottomCenter,
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Positioned(
                     child: Text("Arijit Singh",
                         style: TextStyle(
-                          color: Colors.white,
-                        )))
-          ]),
-        ],
+                            color: Colors.white,
+                            fontSize: 70,
+                            fontWeight: FontWeight.bold))),
+              ),
+            ]),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.shuffle,
+                          color: Colors.green,
+                          size: 25,
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.play_circle,
+                            color: Colors.green,
+                            size: 35,
+                          )),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.end,
+                  ),
+                  UiHelper.customText("Popular", Colors.white, 25),
+                  ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      // return ListTile(
+                      //   leading: Text((index+1).toString()),
+                      //   title: Image.network(
+                      //     arrContent[index]["image"].toString() as String,
+                      //     height: 50,width: 20,fit: BoxFit.fill,
+                      //   ),
+                      //   subtitle: Text(arrContent[index]["songname"].toString()),
+                      //   trailing: Icon(Icons.more_vert),
+                      // );
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            Text((index + 1).toString(),style: TextStyle(color: Colors.white),),
+                            SizedBox(width: 20,),
+                            Image.network(
+                              arrContent[index]["image"].toString() as String,
+                              height: 50,
+                              width: 50,
+                              fit: BoxFit.fill,
+                            ),
+                            SizedBox(width: 15,),
+                            Text(arrContent[index]["songname"].toString(),style: TextStyle(color: Colors.white),),
+                            // SizedBox(width: 20,),
+                            Align(
+                                child: IconButton(onPressed: (){}, icon: Icon(Icons.more_vert),color: Colors.green,),
+                            alignment: Alignment.bottomRight,)
+                            // IconButton(onPressed: (){},icon:
+                            //   Icons.more_vert,
+                            //   color: Colors.green,
+                            // ),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: arrContent.length,
+                  ),
+                  SizedBox(height: 20,),
+                  UiHelper.customText("Popular releases", Colors.white, 25),
+                  ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      // return ListTile(
+                      //   leading: Text((index+1).toString()),
+                      //   title: Image.network(
+                      //     arrContent[index]["image"].toString() as String,
+                      //     height: 50,width: 20,fit: BoxFit.fill,
+                      //   ),
+                      //   subtitle: Text(arrContent[index]["songname"].toString()),
+                      //   trailing: Icon(Icons.more_vert),
+                      // );
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: [
+                            Image.network(
+                              arrContent[index]["image"].toString() as String,
+                              height: 70,
+                              width: 70,
+                              fit: BoxFit.fill,
+                            ),
+                            SizedBox(width: 15,),
+                            Text(arrContent[index]["songname"].toString(),style: TextStyle(color: Colors.white),),
+                            // IconButton(onPressed: (){},icon:
+                            //   Icons.more_vert,
+                            //   color: Colors.green,
+                            // ),
+                          ],
+                        ),
+                      );
+                    },
+                    itemCount: arrContent.length,
+                  ),
+                  // );
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-      // backgroundColor: Colors.black,
+      backgroundColor: Colors.black,
     );
   }
 }
