244ab9014f9b02d9c3707d405ad391f1d65809f6
HP <lavina2daryanani@gmail.com>
2024-07-02 13:13:44 +0530

    initial commit

--- lib/artist.dart
+++ lib/artist.dart
@@ -12,47 +12,45 @@ class _ArtistState extends State<Artist> {
   var arrContent = [
     {
       "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+          "https://m.media-amazon.com/images/I/610FLv2T1QL._AC_UF1000,1000_QL80_.jpg",
       "songname": "Lorem Ipsum1",
       "threedot": Icons.more_vert,
     },
     {
       "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+          "https://m.media-amazon.com/images/I/610FLv2T1QL._AC_UF1000,1000_QL80_.jpg",
       "songname": "Lorem Ipsum1",
       "threedot": Icons.more_vert,
     },
     {
       "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+          "https://m.media-amazon.com/images/I/610FLv2T1QL._AC_UF1000,1000_QL80_.jpg",
       "songname": "Lorem Ipsum1",
       "threedot": Icons.more_vert,
     },
     {
       "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+          "https://m.media-amazon.com/images/I/610FLv2T1QL._AC_UF1000,1000_QL80_.jpg",
       "songname": "Lorem Ipsum1",
       "threedot": Icons.more_vert,
     },
     {
       "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
-      "songname": "Lorem Ipsum1",
-      "threedot": Icons.more_vert,
-    },
-    {
-      "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
-      "songname": "Lorem Ipsum1",
-      "threedot": Icons.more_vert,
-    },
-    {
-      "image":
-          "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
+          "https://m.media-amazon.com/images/I/610FLv2T1QL._AC_UF1000,1000_QL80_.jpg",
       "songname": "Lorem Ipsum1",
       "threedot": Icons.more_vert,
     },
   ];
+  final ScrollController _controller = ScrollController();
+  final double _height = 100.0;
+
+  void _animateToIndex(int index) {
+    _controller.animateTo(
+      index * _height,
+      duration: Duration(seconds: 2),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,8 +65,8 @@ class _ArtistState extends State<Artist> {
             Stack(alignment: Alignment.bottomLeft, children: [
               Container(
                 child: Image.network(
-                  "https://assets.telegraphindia.com/telegraph/2023/Aug/1692339492_arijit-singh.jpg",
-                  fit: BoxFit.fill,
+                  "https://m.media-amazon.com/images/I/610FLv2T1QL._AC_UF1000,1000_QL80_.jpg",
+                  fit: BoxFit.fill,opacity: const AlwaysStoppedAnimation(.7),
                 ),
                 width: double.infinity,
                 height: 200,
@@ -90,6 +88,9 @@ class _ArtistState extends State<Artist> {
                 children: [
                   Row(
                     children: [
+                      TextButton(onPressed: (){}, child:UiHelper.customText("About", Colors.green, 20),),
+                      TextButton(onPressed: (){}, child:UiHelper.customText("Album", Colors.green, 20),),
+                      Spacer(),
                       IconButton(
                         onPressed: () {},
                         icon: Icon(
@@ -106,46 +107,35 @@ class _ArtistState extends State<Artist> {
                             size: 35,
                           )),
                     ],
-                    mainAxisAlignment: MainAxisAlignment.end,
+                    // mainAxisAlignment: MainAxisAlignment.end,
                   ),
+                  SizedBox(height: 10,),
                   UiHelper.customText("Popular", Colors.white, 25),
                   ListView.builder(
                     physics: NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
-                      // return ListTile(
-                      //   leading: Text((index+1).toString()),
-                      //   title: Image.network(
-                      //     arrContent[index]["image"].toString() as String,
-                      //     height: 50,width: 20,fit: BoxFit.fill,
-                      //   ),
-                      //   subtitle: Text(arrContent[index]["songname"].toString()),
-                      //   trailing: Icon(Icons.more_vert),
-                      // );
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Row(
-                          children: [
-                            Text((index + 1).toString(),style: TextStyle(color: Colors.white),),
-                            SizedBox(width: 20,),
-                            Image.network(
-                              arrContent[index]["image"].toString() as String,
-                              height: 50,
-                              width: 50,
-                              fit: BoxFit.fill,
-                            ),
-                            SizedBox(width: 15,),
-                            Text(arrContent[index]["songname"].toString(),style: TextStyle(color: Colors.white),),
-                            // SizedBox(width: 20,),
-                            Align(
-                                child: IconButton(onPressed: (){}, icon: Icon(Icons.more_vert),color: Colors.green,),
-                            alignment: Alignment.bottomRight,)
-                            // IconButton(onPressed: (){},icon:
-                            //   Icons.more_vert,
-                            //   color: Colors.green,
-                            // ),
-                          ],
+                      return ListTile(
+                        leading:
+                        UiHelper.customText((index+1).toString(), Colors.white, 15),
+                        title: Container(
+                          height: 50,
+                          width: 50,
+                          child: Row(
+                            children: [
+                              Image.network(
+                                arrContent[index]["image"].toString() as String,fit: BoxFit.contain,
+                              ),
+                                    SizedBox(width: 15,),
+                              UiHelper.customText(arrContent[index]["songname"].toString(), Colors.white, 20),
+                              // Text(arrContent[index]["songname"].toString()),
+                            ],
+                          ),
                         ),
+                        // subtitle: Text(arrContent[index]["songname"].toString()),
+                        trailing: IconButton(onPressed: (){},icon:Icon(Icons.more_vert),
+                              color: Colors.white,
+                            ),
                       );
                     },
                     itemCount: arrContent.length,
@@ -156,38 +146,31 @@ class _ArtistState extends State<Artist> {
                     physics: NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
-                      // return ListTile(
-                      //   leading: Text((index+1).toString()),
-                      //   title: Image.network(
-                      //     arrContent[index]["image"].toString() as String,
-                      //     height: 50,width: 20,fit: BoxFit.fill,
-                      //   ),
-                      //   subtitle: Text(arrContent[index]["songname"].toString()),
-                      //   trailing: Icon(Icons.more_vert),
-                      // );
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Row(
-                          children: [
-                            Image.network(
-                              arrContent[index]["image"].toString() as String,
-                              height: 70,
-                              width: 70,
-                              fit: BoxFit.fill,
-                            ),
-                            SizedBox(width: 15,),
-                            Text(arrContent[index]["songname"].toString(),style: TextStyle(color: Colors.white),),
-                            // IconButton(onPressed: (){},icon:
-                            //   Icons.more_vert,
-                            //   color: Colors.green,
-                            // ),
-                          ],
+                      return ListTile(
+                        title: Container(
+                          height: 50,
+                          width: 50,
+                          child: Row(
+                            children: [
+                              Image.network(
+                                arrContent[index]["image"].toString() as String,
+                                height: 70,
+                                width: 70,
+                                fit: BoxFit.fill,
+                              ),
+                              SizedBox(width: 15,),
+                              Text(arrContent[index]["songname"].toString(),style: TextStyle(color: Colors.white),),
+                              // Text(arrContent[index]["songname"].toString()),
+                            ],
+                          ),
                         ),
                       );
                     },
                     itemCount: arrContent.length,
                   ),
-                  // );
+                  Container(
+                    child: Text("About"),
+                  )
                 ],
               ),
             )
